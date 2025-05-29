5ee00551e713534479d5ec8cc13528588a77a4ab
Gharzof <gharzof@gmail.com>
2025-05-29 18:00:56 +0500

    Add whitenoise storage

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,8 @@ AUTH_USER_MODEL = 'core.User'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
